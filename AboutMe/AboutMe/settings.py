e4d54c6bd26fdc8f025986efc2e8e0d32b3dc2a9
Abi Developer <abinaya.saravanan1105@gmail.com>
2014-12-21 19:46:48 +0530

    Background and tabs ready

--- AboutMe/AboutMe/settings.py
+++ AboutMe/AboutMe/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+STATIC_PATH = os.path.join(BASE_DIR,'static')
 
 SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
 # Quick-start development settings - unsuitable for production
@@ -84,3 +85,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    STATIC_PATH,
+)
